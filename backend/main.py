d04c6420e7225a143f2335fab19d0215874b8a78
DonedaDiego <117370206+DonedaDiego@users.noreply.github.com>
2025-06-17 15:51:31 -0300

    denovo v2

--- backend/main.py
+++ backend/main.py
@@ -874,7 +874,7 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     debug_mode = os.environ.get("FLASK_ENV") == "development"
     
-    print("🚀 Iniciando Geminii API (DESENVOLVIMENTO)...")git a
+    print("🚀 Iniciando Geminii API (DESENVOLVIMENTO)...")
     print("⚠️  Para produção, use Gunicorn!")
     
     app.run(host='0.0.0.0', port=port, debug=debug_mode)
